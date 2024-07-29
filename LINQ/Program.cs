af6b14336a8bbbc2baf41756200212ac65e6bc06
StewartBaker <bakrstewart16@gmail.com>
2024-07-29 11:43:29 -0500

    finished exercise

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videoGames = new List<string>() { "Fallout New Vegas", "Warframe", "Prey", "Dark Souls 3", "Elden Ring", "Overwatch"};
+
+            var orderedVideoGames = videoGames.OrderBy(name => name.Length);
+            var i = 0;
+            foreach (var game in orderedVideoGames)
+            {
+                i++;
+                Console.WriteLine($"{i}. {game}");
+            }
         }
     }
 }
